de484634e1546acd2ada17f1f79518715e68a6c8
Praveen Dias <praveendias1180@gmail.com>
2022-02-27 10:38:24 +0530

    Courses endpoint completed 💇

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+]
+
 app.get('/',  (req, res) => {
     res.send('Hello World')
 })
 
 app.get('/api/courses/',  (req, res) => {
-    res.send('Course List Display')
+    res.send(courses)
 })
 
-app.get('/api/courses/:id/:name', (req, res) => {
-    // res.send(req.params)
-    res.send(req.query)
+app.get('/api/courses/:id/', (req, res) => {
+    res.send(courses[req.params.id])
 })
 
 // export PORT=3400
