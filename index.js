756a6781fdc10d66d7e8c54da647b321683a9123
Praveen Dias <praveendias1180@gmail.com>
2022-02-27 10:29:29 +0530

    Route parameters

--- index.js
+++ index.js
@@ -9,5 +9,11 @@ app.get('/api/courses/',  (req, res) => {
     res.send('Course List Display')
 })
 
+app.get('/api/courses/:id', (req, res) => {
+    res.send('id is ' + req.params.id)
+})
+
+// export PORT=3400
+// set PORT=3400
 const port = process.env.PORT || 3300
 app.listen(port, () => console.log(`Listening on port ${port}...`))
